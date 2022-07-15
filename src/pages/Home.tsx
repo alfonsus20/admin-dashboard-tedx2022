4d99fa3446f9acb9c177c4668b4dd5ec5a1f83a7
alfonsus20 <alfonschandrawan@gmail.com>
2022-07-15 10:50:19 +0700

    init repo

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Flex, Heading } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <Flex alignItems='center' justifyContent="center" minH='100vh' >
+      <Heading mb={4}>ADMIN DASHBOARD COMING SOON</Heading>
+    </Flex>
+  );
+};
+export default Home;
