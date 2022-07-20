0c790de292a6e19eda7b25dafc3c015cdcf36dbc
alfonsus20 <alfonschandrawan@gmail.com>
2022-07-20 08:51:22 +0700

    auto logout when token is invalid

--- src/hooks/useError.ts
+++ src/hooks/useError.ts
@@ -1,13 +1,23 @@
 import { useToast } from "@chakra-ui/react";
 import { AxiosError } from "axios";
+import { useUserContext } from "../context/UserContext";
 
 function useError() {
   const snackbar = useToast();
+  const { logout } = useUserContext();
+
   const handleError = (err: unknown, message: string = "Terjadi Kesalahan") => {
-    snackbar({
-      title: err instanceof AxiosError ? err.response?.data.message : message,
+    const errorMessage =
+      err instanceof AxiosError ? err.response?.data.message : message;
+    
+      snackbar({
+      title: errorMessage,
       status: "error",
     });
+
+    if (errorMessage === "Invalid token") {
+      logout();
+    }
   };
 
   return { handleError };
